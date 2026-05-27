5bdbafd94fffa3d7ae2e5c1c9b5c85bcd49153a2
Sunil1532 <sunilthati27cs@gmail.com>
2026-05-27 18:51:27 +0530

    point frontend to render backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://breathe-esg-5y94.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
